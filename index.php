11b3e6b7c3aaf8e810fe00d91c2c8a10e7c96c63
Vilius Senkus <vilius.s@outlook.com>
2023-01-30 15:24:28 +0200

    navigation to html file added. Possibility to create files and folders added.

--- index.php
+++ index.php
@@ -1,13 +1,13 @@
 <?php
-$folder="./";
+$folder = "./";
 if (isset($_GET['folder'])) {
 
       $split = (explode("/", $_GET['folder']));
 
-      if ($split[count($split)-2]=="."){
-            array_splice($split,(count($split)-3),2);
+      if ($split[count($split) - 2] == ".") {
+            array_splice($split, (count($split) - 3), 2);
             $folder = implode("/", $split);
-      }else{
+      } else {
             $folder = $_GET['folder'];
       }
 
@@ -15,26 +15,22 @@ if (isset($_GET['folder'])) {
 
 //Folderio duomenų paėmimas ir išrūšiavimas
 $folderData = scandir($folder);
-$folderData = _sortData($folderData);
-function _sortData($dataArr){
-      foreach ($dataArr as $data) {
-            if (is_dir($data)) {
-                  $foldersArr[] = $data;
-            } else {
-                  $filesArr[] = $data;
-            }
-      }
-      $sortedArr = array_merge($foldersArr, $filesArr);
-      return $sortedArr;
-}
+// $folderData = _sortData($folderData);
+// function _sortData($dataArr)
+// {
+//       foreach ($dataArr as $data) {
+//             if (is_dir($data)) {
+//                   $foldersArr[] = $data;
+//             } else {
+//                   $filesArr[] = $data;
+//             }
+//       }
+//       $sortedArr = array_merge($foldersArr, $filesArr);
+//       return $sortedArr;
+// }
 
 //ikonų sudėjimui iš interneto "https://img.icons8.com/external-fauzidea-flat-fauzidea/32/null/external-php-file-file-extension-fauzidea-flat-fauzidea.png", "r");
 
-
-//failo sukūrimas
-if (isset($_POST['fileName']) && $_POST['fileName'] != "") {
-      file_put_contents($folder . "/" . $_POST['fileName'], $_POST['fileContent']);
-}
 ?>
 
 <!DOCTYPE html>
@@ -60,20 +56,58 @@ if (isset($_POST['fileName']) && $_POST['fileName'] != "") {
                   <div>Build to meet CRUD functionality</div>
                   <nav>
                         <ul>
-                              <li>New Directory</li>
-                              <li>New File</li>
+                              <li name="nfo">New Directory</li>
+                              <li name="nfi">New File</li>
                         </ul>
                   </nav>
             </header>
 
             <main>
+                  <div class="new_file">
+                        <form method="POST">
+                              <div class="inputField">
+                                    <label>File Name</label>
+                                    <input type="text" name="fileName" />
+                              </div>
+                              <div class="inputField">
+                                    <label>File content</label>
+                                    <textarea type="text" name="fileContent"></textarea>
+                              </div>
+                              <button type="submit">Create</button>
+                        </form>
+                  </div>
+
+                  <div class="new_folder">
+                        <form method="POST">
+                              <div class="inputField">
+                                    <label>Folder Name</label>
+                                    <input type="text" name="folderName" />
+                              </div>
+                              <button type="submit">Create</button>
+                        </form>
+                  </div>
+
+                  <?php
+                  if (isset($_POST['fileName']) && $_POST['fileName'] != "") {
+                        if (!isset($_POST['fileContent'])) {
+                              $_POST['fileContent'] = "";
+                        }
+                        file_put_contents($folder .$_POST['fileName'], $_POST['fileContent']);
+                        header('Location:' . $_SERVER['REQUEST_URI']);
+                  }
+                  if (isset($_POST['folderName']) && $_POST['folderName'] != "") {
+                        mkdir($folder . $_POST['folderName']);
+                        header('Location:' . $_SERVER['REQUEST_URI']);
+                  }
+                  ?>
+
                   <div>
-                        Now you are 
-                        <?php 
-                        if ($folder=="./"){
+                        Now you are
+                        <?php
+                        if ($folder == "./") {
                               echo "home";
-                        }else{
-                        echo "here: $folder";
+                        } else {
+                              echo "here: $folder";
                         }
                         ?>
                   </div>
@@ -100,14 +134,14 @@ if (isset($_POST['fileName']) && $_POST['fileName'] != "") {
                                                 <input type="checkbox" name="check">
                                           </td>
 
-                                          <td>    
-                                    <!-- deciding if $data is folder or file and performing adequate actions -->
-                                          <?php
-                                                if (is_dir($folder.$data)) { ?>
+                                          <td>
+                                                <!-- deciding if $data is folder or file and performing adequate actions -->
+                                                <?php
+                                                if (is_dir($folder . $data)) { ?>
                                                       <div class="icon">
                                                             <img src="https://img.icons8.com/emoji/32/null/file-folder-emoji.png" />
                                                       </div>
-                                                      <a href="?folder=<?= $folder.$data ?>/"><?= $data; ?></a>
+                                                      <a href="?folder=<?= $folder . $data ?>/"><?= $data; ?></a>
                                                       <?php
 
                                                 } else {
@@ -124,24 +158,26 @@ if (isset($_POST['fileName']) && $_POST['fileName'] != "") {
                                                       </div>
                                                       <?php echo $data;
                                                 }
-                                          ?>
+                                                ?>
                                           </td>
 
                                           <td>
-                                          <?php
-                                          // counting and representing size in more acceptable format
-                                                $size=filesize($folder.$data);
+                                                <?php
+                                                // counting and representing size in more acceptable format
+                                                $size = filesize($folder . $data);
                                                 if ($size < 1000) {
-                                                      echo $size."&nbsp;&nbsp;B";
-                                                }elseif($size >= 1000 AND $size<1000000){
-                                                      echo round(($size/1000),2,PHP_ROUND_HALF_UP)." KB";
-                                                }else{
-                                                      echo round(($size / 1000000),2,PHP_ROUND_HALF_UP) . " GB";
+                                                      echo $size . "&nbsp;&nbsp;B";
+                                                } elseif ($size >= 1000 and $size < 1000000) {
+                                                      echo round(($size / 1000), 2, PHP_ROUND_HALF_UP) . " KB";
+                                                } else {
+                                                      echo round(($size / 1000000), 2, PHP_ROUND_HALF_UP) . " GB";
                                                 }
-                                          ?>
-                                                      
+                                                ?>
+
                                           </td>
-                                          <td><?php echo date("Y-m-d", filemtime($folder.$data)); ?></td>
+                                          <td>
+                                                <?php echo date("Y-m-d", filemtime($folder . $data)); ?>
+                                          </td>
                                           <td>
                                                 <button>delete</button>
                                                 <button>rename</button>
@@ -150,26 +186,6 @@ if (isset($_POST['fileName']) && $_POST['fileName'] != "") {
                               <?php } ?>
                         </tbody>
                   </table>
-
-                  <form method="POST">
-                        <div class="inputField">
-                              <label>File Name</label>
-                              <input type="text" name="fileName" />
-                        </div>
-                        <div class="inputField">
-                              <label>File content</label>
-                              <textarea type="text" name="fileContent"></textarea>
-                        </div>
-                        <button>Create</button>
-                  </form>
-
-                  <form method="POST">
-                        <div class="inputField">
-                              <label>Folder Name</label>
-                              <input type="text" name="folderName" />
-                        </div>
-                        <button>Create</button>
-                  </form>
             </main>
 
             <footer>
@@ -185,6 +201,22 @@ if (isset($_POST['fileName']) && $_POST['fileName'] != "") {
                         </ul>
                   </div>
             </footer>
+
+            <script>
+                  document.querySelector('[name="nfo"]').addEventListener("click", () => {
+                        if (document.querySelector('.new_file').style.display = "block") {
+                              document.querySelector('.new_file').style.display = "none";
+                        }
+                        document.querySelector('.new_folder').style.display = "block";
+                  });
+                  document.querySelector('[name="nfi"]').addEventListener("click", () => {
+                        if (document.querySelector('.new_folder').style.display = "block") {
+                              document.querySelector('.new_folder').style.display = "none";
+                        }
+                        document.querySelector('.new_file').style.display = "block";
+                  });
+
+            </script>
 
       </div>
 </body>
