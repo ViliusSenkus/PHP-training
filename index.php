1e8b022d6dd30053d88024f80f727b93692e2ddb
Vilius Senkus <vilius.s@outlook.com>
2023-01-31 16:14:17 +0200

    hard trying - zero result

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ if (isset($_GET['folder'])) {
 
 }
 
-//Sorting/Ordering Folders then Files:
+//Ordering Folders then Files:
 $folderData = scandir($folder);
 
 if (count($folderData) > 1) {
@@ -211,13 +211,13 @@ Alternative web icons:
                                                       </svg>
 
                                                 
-
+                                                <a href="?edit=<?= $folder.$data ?>&folder=<?= $folder ?>">
                                                       <svg name="ren" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
                                                             <path fill="#5588EE" d="M0 64C0 28.7 28.7 0 64 0H224V128c0 17.7 14.3 32 32 32H384V285.7l-86.8 86.8c-10.3 10.3-17.5 23.1-21 37.2l-18.7 74.9c-2.3 9.2-1.8 18.8 1.3 27.5H64c-35.3 0-64-28.7-64-64V64zm384 64H256V0L384 128zM549.8 235.7l14.4 14.4c15.6 15.6 15.6 40.9 0 56.6l-29.4 29.4-71-71 29.4-29.4c15.6-15.6 40.9-15.6 56.6 0zM311.9 417L441.1 287.8l71 71L382.9 487.9c-4.1 4.1-9.2 7-14.9 8.4l-60.1 15c-5.5 1.4-11.2-.2-15.2-4.2s-5.6-9.7-4.2-15.2l15-60.1c1.4-5.6 4.3-10.8 8.4-14.9z"/>
                                                       </svg>
-
+                                                </a>
                                                 
-                                                <svg name="copy" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                                                      <svg name="copy" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
                                                       <path fill="#666666" d="M0 448c0 35.3 28.7 64 64 64H288c35.3 0 64-28.7 64-64V384H224c-53 0-96-43-96-96V160H64c-35.3 0-64 28.7-64 64V448zm224-96H448c35.3 0 64-28.7 64-64V64c0-35.3-28.7-64-64-64H224c-35.3 0-64 28.7-64 64V288c0 35.3 28.7 64 64 64z"/>
                                                 </svg>
                                           </td>
