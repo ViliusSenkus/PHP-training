afab0e89de1f46aa6589559aba048e0e8d2859d6
Vilius Senkus <vilius.s@outlook.com>
2023-02-15 16:28:08 +0200

    recievinuing data of latest adds in to main page functionality added

--- SQL_training/2023-02-14_musicbox/view/main.php
+++ SQL_training/2023-02-14_musicbox/view/main.php
@@ -37,22 +37,51 @@ if (!empty($_SESSION) && $_SESSION['user'] != ""){
       //Not Connected
       
 }else{
-      echo "<h2>Our music selection</h2>";
+      //data preparation
+    
+
+      echo "<h2>Our most recently added music tracks</h2>";
+      $counter=0;
       for($i=1; $i<3; $i++){
             ?>
             <div class="row">
             <?php
             for($z=1; $z<5; $z++){
-            ?>
-                 <div class="album_box">
-                 </div>
-            <?php
+            
+                        $sqlrequest = $sql->query("SELECT MAX(id)-$counter FROM `songs`");
+                        $maxid=($sqlrequest->fetch_row())[0];
+                        $sqlrequest = $sql->query("SELECT MIN(id) FROM `songs`");
+                        $minid=($sqlrequest->fetch_row())[0];
+                        $sqlsong = $sql->query("SELECT * FROM songs WHERE id = $maxid");
+                        $songdata=($sqlsong->fetch_row());
+                        if ($songdata){
+                              if ($songdata[0]==$minid){
+                                    $counter--;
+                              } 
+                              ?>
+                              <div class="album_box">
+                               <?php
+                                    echo "<pre>";
+                                    print_r($songdata[0]);
+                                    echo "</pre>";
+                                    $counter++;                                   
+                              ?>
+                                    </div>
+                              <?php
+                        }else{
+                              $z--;
+                              $counter++;
+                        }
             }
             ?>
             </div>
       <?php
       }
 }
+echo "<h2>Sign up now and enjoy melody</h2>";
+
+
+
 
 ?>
 </main>
