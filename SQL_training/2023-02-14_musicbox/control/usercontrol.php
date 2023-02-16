cfd07286c056ceb410efb38838d4bde9f09afb63
Vilius Senkus <vilius.s@outlook.com>
2023-02-16 22:23:54 +0200

    adding to play list done

--- SQL_training/2023-02-14_musicbox/control/usercontrol.php
+++ SQL_training/2023-02-14_musicbox/control/usercontrol.php
@@ -1,7 +1,23 @@
 <?php
-if (isset ($_SESSION['user']) && $_SESSION['user']!="" && isset($_GET['userdat']) && $_GET['userdat'] != ""){
-      echo "add to playlist";
-      print_r($songdata);
+
+// adding to playlist
+
+// !!!!!!!!!!!!!reikalinga padaryti forma pasirinkimui i koki playlista deti!!!!!!!!!!!!!!!!!
+if (isset($_GET['userad']) && $_GET['userad'] != ""){
+      $songid=$_GET['userad'];
+      $nickname=$_SESSION['user'];
+      $sqlrequest=($sql->query("SELECT playlists FROM users WHERE nickname='$nickname'"));
+      $playlist=($sqlrequest->fetch_row())[0];
+
+      if($playlist != null){
+            $plList=json_decode($playlist, true);
+            $plList[]=$songid;
+      }else{
+            $plList=array($songid);      
+      }
+      $json=json_encode($plList);
+      $sqlrequest=$sql->query("UPDATE users SET playlists='$json' WHERE nickname='$nickname'");  
+      echo "<br />added to playlist";
      }
      
 
@@ -24,7 +40,6 @@ if (isset($_GET['fav']) && $_GET['fav'] != ""){
       }
       $json=json_encode($favList);
       $sqlrequest=$sql->query("UPDATE users SET favorites='$json' WHERE nickname='$nickname'");  
-      
       echo "<br />added to favorites";
 }
 
