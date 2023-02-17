0f247323c9826e4a78f5238f5651fd4c90b4e04a
Vilius Senkus <vilius.s@outlook.com>
2023-02-17 12:02:43 +0200

    some input field styling and data to pass through form added

--- SQL_training/2023-02-14_musicbox/control/usercontrol.php
+++ SQL_training/2023-02-14_musicbox/control/usercontrol.php
@@ -8,13 +8,17 @@ if (isset($_GET['userad']) && $_GET['userad'] != ""){
 
       $songid=$_GET['userad'];
       $nickname=$_SESSION['user'];
+      $sqlrequest=($sql->query("SELECT * FROM songs WHERE id='$songid'"));
+      $songdata=$sqlrequest->fetch_all()[0];
+      $songname=$songdata[1]." - ".$songdata[2];
+
       $sqlrequest=($sql->query("SELECT playlists FROM users WHERE nickname='$nickname'"));
       $playlist=($sqlrequest->fetch_row())[0];
       ?>
 
+      <!-- FORM to add song to Playlist -->
       <form method="POST">
-            <label>Song</label>
-            <input type=text disabled name="songtoupload" value="song">
+            <input type=text disabled name="songtoupload" value="<?=$songname?>" id="songinputname">
             <label>Select playlist to add to</label>
             <select name="plst" id="pllist">
                   <option value=favorites>Favorites</option>
@@ -22,6 +26,7 @@ if (isset($_GET['userad']) && $_GET['userad'] != ""){
                   <option value=readSQL>readSQL</option>
             </select>
             <input type="hidden" name="song" />
+            <input type="hidden" name="songid" value='<?=$songid?>'"/>
             <div id="newplname" style="display:none">
                   <label>Give name to new playlist</label>
                   <input type="text" name="userplalist" />
@@ -39,12 +44,6 @@ if (isset($_GET['userad']) && $_GET['userad'] != ""){
                   }
 
             }
-            
-            // function pagavimas(){
-            //       console.log('pasikeite');
-            // }
-
-            // document.querySelector('#pllist').addEventListener('change', pagavimas() );
       </script>
 
       
@@ -52,7 +51,7 @@ if (isset($_GET['userad']) && $_GET['userad'] != ""){
 }
 
 
-if (isset($_POST['songtoupload']) && $_POST['songtoupload'] == "song" ){
+if (isset($_POST['songtoupload']) && $_POST['songtoupload'] != "" ){
       echo "Per forma pridedama daina";
 
 
