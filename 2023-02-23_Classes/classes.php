fc43e41be3a68e5967b65929aa9a14af983a6d1a
Vilius Senkus <vilius.s@outlook.com>
2023-02-24 12:13:26 +0200

    HW sql added

--- 2023-02-23_Classes/classes.php
+++ 2023-02-23_Classes/classes.php
@@ -6,11 +6,11 @@ class Avia {
       const PSW='';
       const DB='avia';
 
-      public $db="";
+      public static $db="";
 
       function __constructor(){
             try{
-                  $db=new mysqli(self::HOST, self::USER, self::PSW, self::DB);
+                  self::$db=new mysqli(self::HOST, self::USER, self::PSW, self::DB);
             }catch(Exception $error){
                   echo "<h2>DB service is not avialable</h2><br />";
                   echo $error;
@@ -19,9 +19,45 @@ class Avia {
       }
 
       function addFlight($from, $to, $fl_num, $fl_date ){
-            $sql=$db->query("INSERT INTO flights(f_from, f_to, flight_number, flight_date) VALUES ('$from','$to','$fl_num','$fl_date')")
+            self::$db->query("INSERT INTO flights(f_from, f_to, flight_number, flight_date) VALUES ('$from','$to','$fl_num','$fl_date')");
       }
 
+      public $flights=[];
+      public $passengers=[];
+      public $flightdata=[];
+
+      function showflights(){
+            $this->flights=self::$db->query("SELECT * FROM fligths");
+            return $this;
+      }
+
+      function sortfligthsfrom($sorttype="ASC"){
+            $this->flights=self::$db->query("SELECT * FROM flights ORDER BY f_from $sorttype");
+            return $this;
+      }
+
+      function sortfligthsto($sorttype="ASC"){
+            $this->flights=self::$db->query("SELECT * FROM flights ORDER BY f_to $sorttype");
+            return $this;
+      }
+
+      function passangersinflight($fl_num){
+            $this->passengers=self::$db->query("SELECT * FROM passengers WHERE flight_id='$fl_num'");
+            return $this;
+      }
+
+      function passengerflightdata($name, $surname){
+            $this->flightdata=self::$db->query("SELECT f.*, p.* FROM flights AS f JOIN passengers AS p ON p.flight_id = f.flight_number WHERE p.first_name='$name' AND p.last_name='$surname'");
+            return $this;
+      }
 
 }
 ?>
+
+
+
+Filtracijas atlikinėkite pasinaudodami PHP, o ne SQL funkcionalumu.
+
+
+
+Aprašykite klasę atsakingą už šių duomenų tvarkymą ir rezultatų grąžinimą.
