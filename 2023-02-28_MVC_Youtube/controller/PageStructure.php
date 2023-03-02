d40596ccfcecccf0c60bea061f7e1b442f4fe26f
Vilius Senkus <vilius.s@outlook.com>
2023-03-02 21:09:50 +0200

    code for actualbar added, need to track info flow for corect code positioning

--- 2023-02-28_MVC_Youtube/controller/PageStructure.php
+++ 2023-02-28_MVC_Youtube/controller/PageStructure.php
@@ -2,15 +2,21 @@
 
 namespace Controller;
 class PageStructure{
-                  
-      public static function mainSpace($part="main"){
-            include "view/".$part.".php";
-      }
-
-      public static function getSidebarMenu(){
+            public static function getSidebarMenu(){
             $sidebar = new \Model\Elements\Sidebar();
             $sidebar = $sidebar->get();
             include "view/sidebar.php";
+      }        
+      public static function mainSpace($part="main"){
+            include "view/".$part.".php";
+            self::getActualBar();
+      }
+
+    
+
+      public static function getActualBar(){
+            $actualbar = new \Model\Categories();
+            $actualbar = $actualbar->get();
       }
 }
                   
--- 2023-02-28_MVC_Youtube/view/main/actual.php
+++ 2023-02-28_MVC_Youtube/view/main/actual.php
@@ -7,6 +7,23 @@
 <div class="actual-cat">
       <nav>
             <ul>
+
+<?php
+
+      foreach($actualbar as $a) :
+            if ($a['actual']===1) :
+
+?>
+                  <li>
+                        <a href="?category=<?=$a['id']?>"><?=$a['category']?></a>
+                  </li>
+                  
+<?php
+
+                  endif;
+            endforeach;
+
+?>
                   <li>
                         <a href="?category=">Kategorija</a>
                   </li>
